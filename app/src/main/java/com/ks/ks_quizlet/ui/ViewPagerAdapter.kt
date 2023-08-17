4fcb0caf8c263565ce8f9111e4183ee5c1a70e46
Joseph <j.t.cartoonist@gmail.com>
2023-08-17 20:54:57 +0100

    added dummy data to simulate question experience and ssave the result but there's an issue with maintaining the state of the answers

--- app/src/main/java/com/ks/ks_quizlet/ui/ViewPagerAdapter.kt
+++ app/src/main/java/com/ks/ks_quizlet/ui/ViewPagerAdapter.kt
@@ -1,16 +1,14 @@
 package com.ks.ks_quizlet.ui
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.RadioGroup
-import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.children
-import androidx.core.view.forEach
 import androidx.viewpager.widget.PagerAdapter
 import com.ks.ks_quizlet.R
 
@@ -18,31 +16,31 @@ class ViewPagerAdapter(private val context: Context) : PagerAdapter() {
     var layoutInflater: LayoutInflater? = null
 
 
-    private val descArray = arrayOf(
+    private val options = listOf(
         "Track Your Phone3",
         "Lock Your Phone4",
         "Lock Your Phone46",
         "Ring Your Device"
     )
-    private val titleArray = arrayOf(
-        "Stay Connected, Never Lost: Unlock the Power to Effortlessly Track Your Phone and Always Stay in Control of Your Device's Location!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data2!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data!3",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data4!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data5!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data6!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data!7",
-        /*"Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data4!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data!4",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data5!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data56!",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data!67",
-        "Secure Your World, Anytime, Anywhere: Empower Yourself to Easily Lock Your Phone and Safeguard Your Personal Data!67",*/
-        "Find Your Lost Phone in a Jiffy: Ring Your Phone with Confidence  and Never Let It Wander Far From Your Reach!"
+    private val questions = mutableMapOf<String, Map<String, List<String>>>(
+        "what is your name?" to mapOf("unknown" to options.shuffled()),
+        "what class are you ?" to mapOf("ion know" to options.shuffled()),
+        "Any Hobby at all?" to mapOf("dunno" to options.shuffled()),
+        "Are you young?" to mapOf("yh" to options.shuffled()),
+        "Are you smart?" to mapOf("pretty much" to options.shuffled()),
+        "Are you ambitious?" to mapOf("not sure" to options.shuffled()),
+        "Are you a geek?" to mapOf("no" to options.shuffled()),
+        "Do you like coding?" to mapOf("yes" to options.shuffled())
     )
 
+    private val keyQuestions = questions.keys.toList()
+    private val answerValues = questions.values.toList()
+
+    private val answerList = mutableMapOf<Int,String>()
+
+
     override fun getCount(): Int {
-        return titleArray.size
+        return keyQuestions.size
     }
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
@@ -54,15 +52,25 @@ class ViewPagerAdapter(private val context: Context) : PagerAdapter() {
         val view = layoutInflater!!.inflate(R.layout.questions_layout, container, false)
         val questionTxt = view.findViewById<TextView>(R.id.question_text)
         val answerGrp = view.findViewById<RadioGroup>(R.id.radioGroup)
+        val answerBtn1 = view.findViewById<RadioButton>(R.id.radio_button_1)
+        val answerBtn2 = view.findViewById<RadioButton>(R.id.radio_button_2)
+        val answerBtn3 = view.findViewById<RadioButton>(R.id.radio_button_3)
+        val answerBtn4 = view.findViewById<RadioButton>(R.id.radio_button_4)
 
-        answerGrp.children.forEach {
-            val answerBtn = it as RadioButton
-            descArray.forEach {answer->
-                answerBtn.text = answer
-            }
+
+        questionTxt.text = keyQuestions[position]
+        answerBtn1.text = answerValues[position].values.toList()[0][0]
+        answerBtn2.text = answerValues[position].values.toList()[0][1]
+        answerBtn3.text = answerValues[position].values.toList()[0][2]
+        answerBtn4.text = answerValues[position].values.toList()[0][3]
+
+        answerGrp.setOnCheckedChangeListener { radioGroup, i ->
+            val one = radioGroup.findViewById<View>(i) as RadioButton
+            Log.d("QUESTION", "instantiateItem: ${one.text}, ${one.isChecked}")
+            answerList[keyQuestions.indexOf(questionTxt.text)] = one.text.toString()
+            Log.d("QUESTION", "instantiateItem: ${answerList}")
 
         }
-        questionTxt.text = titleArray[position]
         container.addView(view)
         return view
 
